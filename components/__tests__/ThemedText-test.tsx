27e81ad52699503ff29df405d58ec69855966568
Henry Okonkwo <henry@artnight.com>
2024-09-20 20:22:03 +0200

    chore: Setup testing with vitest uninstall jest dependencies and install vitest dependencies adds vitest.config.mts file adds setuTest.ts file refactor ThemedText-test.tsx setup coverage with instabul setup github actions ci workflow build.yml file

--- components/__tests__/ThemedText-test.tsx
+++ components/__tests__/ThemedText-test.tsx
@@ -1,10 +1,10 @@
-import * as React from 'react';
-import renderer from 'react-test-renderer';
-
+import { render, screen } from '@testing-library/react'
 import { ThemedText } from '../ThemedText';
 
-it(`renders correctly`, () => {
-  const tree = renderer.create(<ThemedText>Snapshot test!</ThemedText>).toJSON();
-
-  expect(tree).toMatchSnapshot();
-});
+describe('App', () => {
+  it('renders the App component', () => {
+    render(<ThemedText />)
+    
+    screen.debug();
+  })
+})
